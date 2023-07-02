aed08ab9e3d6c3fb1b999a1c6c575e3dc6017651
Wanjohi-Erick <wanjohierick07@gmail.com>
2023-07-02 22:02:38 +0300

    treatment transactions record

--- database/migrations/2023_06_13_185150_animal_treatment.php
+++ database/migrations/2023_06_13_185150_animal_treatment.php
@@ -10,22 +10,27 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('animal_treatment', function (Blueprint $table) {
-            $table->id();
-            $table->string('type');
-            $table->string('product');
-            $table->string('application_method');
-            $table->integer('days_until_withdrawal');
-            $table->string('technician');
-            $table->integer('dosage');
-            $table->date('treatment_date');
-            $table->string('body_part');
-            $table->date('booster_date');
-            $table->float('total_cost');
-            $table->text('description')->nullable();
-            $table->timestamps();
-        });
+    {Schema::create('animal_treatment', function (Blueprint $table) {
+        $table->id();
+        $table->string('type');
+        $table->string('product');
+        $table->string('application_method');
+        $table->integer('days_until_withdrawal');
+        $table->string('technician');
+        $table->integer('dosage');
+        $table->date('treatment_date');
+        $table->string('body_part');
+        $table->date('booster_date')->nullable();
+        $table->double('total_cost', 8, 2);
+        $table->text('description')->nullable();
+        $table->timestamp('created_at')->nullable();
+        $table->timestamp('updated_at')->nullable();
+        $table->unsignedBigInteger('farm')->default(0);
+        $table->unsignedBigInteger('animal_id');
+
+        $table->foreign('animal_id')->references('id')->on('all_animals')->onDelete('cascade')->onUpdate('cascade');
+        $table->foreign('farm')->references('id')->on('farm_details')->onDelete('cascade')->onUpdate('cascade');
+    });
     }
 
     /**
